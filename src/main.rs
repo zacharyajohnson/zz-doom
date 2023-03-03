ad88e76fb55b53cb770af41cf9b1b82685260638
Zachary Alan Johnson <zjohnson366@gmail.com>
2023-03-02 21:01:00 -0500

    Add logic to support @responsefile option

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,9 @@ use option::DoomOptions;
 fn main() {
     // Skipping the first arg as this is the executable name
     // and we don't want that
-    //TODO Figure out how to condense this into one step
-    let cmd_args_string: Vec<String> = env::args().skip(1).collect();
-    let cmd_args_str = cmd_args_string.iter().map(AsRef::as_ref).collect();
+    let cmd_args: Vec<String> = env::args().skip(1).collect();
 
     let mut doom_options: DoomOptions = DoomOptions::new();
 
-    option::set_options(&mut doom_options, cmd_args_str);
+    option::set_options(&mut doom_options, cmd_args);
 }
--- src/option.rs
+++ src/option.rs
@@ -194,7 +194,7 @@ pub fn get_option_by_name<'a>(
     return None;
 }
 
-pub fn get_option_by_name_mut<'a>(
+fn get_option_by_name_mut<'a>(
     doom_options: &'a mut DoomOptions,
     option_name: &str,
 ) -> Option<&'a mut DoomOption> {
@@ -207,10 +207,49 @@ pub fn get_option_by_name_mut<'a>(
     return None;
 }
 
-pub fn set_options(doom_options: &mut DoomOptions, cmd_args: Vec<&str>) {
-    let mut cmd_arg_index = 0;
-    while cmd_arg_index < cmd_args.len() {
-        let option_name: &str = cmd_args[cmd_arg_index];
+fn get_response_file_options(file_path: &str) -> Vec<String> {
+    let lines: Vec<String> = std::fs::read_to_string(file_path)
+        .expect(&format!("Unable to find response file {}", file_path))
+        .lines()
+        .map(|x| x.to_owned())
+        .collect();
+
+    lines
+        .iter()
+        .filter(|x| {
+            x.chars().all(|c| {
+                if c.is_ascii() {
+                    c >= ' ' && c <= 'z'
+                } else {
+                    false
+                }
+            })
+        })
+        .flat_map(|line| line.split_ascii_whitespace().map(|x| x.to_owned()))
+        .collect()
+}
+
+pub fn set_options(doom_options: &mut DoomOptions, cmd_args: Vec<String>) {
+    let mut arg_index = 0;
+
+    let args_to_process = match cmd_args.iter().position(|x| x.starts_with("@")) {
+        Some(i) => {
+            let mut response_file_args: Vec<String> =
+                get_response_file_options(cmd_args[i].trim_start_matches("@"));
+
+            // Grab all the args after response and put it into the args list
+            // to process since thats how the original game behaves
+            if i < cmd_args.len() - 1 {
+                response_file_args.extend_from_slice(&cmd_args[i + 1..]);
+            }
+
+            response_file_args
+        }
+        None => cmd_args,
+    };
+
+    while arg_index < args_to_process.len() {
+        let option_name: &str = &args_to_process[arg_index];
         let option: Option<&mut DoomOption> = get_option_by_name_mut(doom_options, option_name);
 
         match option {
@@ -219,15 +258,15 @@ pub fn set_options(doom_options: &mut DoomOptions, cmd_args: Vec<&str>) {
                 let min_num_values = i.min_num_values;
                 let max_num_values = i.max_num_values;
 
-                cmd_arg_index += 1;
+                arg_index += 1;
 
                 if max_num_values == 0 {
                     option_value.push_str("true");
                 } else {
                     let mut value_index = 0;
 
-                    while cmd_arg_index != cmd_args.len()
-                        && !cmd_args[cmd_arg_index].starts_with("-")
+                    while arg_index != args_to_process.len()
+                        && !args_to_process[arg_index].starts_with("-")
                     {
                         value_index += 1;
 
@@ -235,12 +274,12 @@ pub fn set_options(doom_options: &mut DoomOptions, cmd_args: Vec<&str>) {
                             panic!("Too many values provided for option {}. Requires min of {} values and max of {} values, but {} supplied.", i.name, min_num_values, max_num_values, value_index);
                         }
 
-                        option_value.push_str(cmd_args[cmd_arg_index]);
-                        cmd_arg_index += 1;
+                        option_value.push_str(&args_to_process[arg_index]);
+                        arg_index += 1;
 
                         //TODO This is ugly
-                        if cmd_arg_index != cmd_args.len()
-                            && !cmd_args[cmd_arg_index].starts_with("-")
+                        if arg_index != args_to_process.len()
+                            && !args_to_process[arg_index].starts_with("-")
                         {
                             option_value.push_str(" ");
                         }
@@ -268,35 +307,37 @@ mod tests {
     #[test]
     fn test_doom_options_intializes_with_default_doom_options() {
         let doom_options: DoomOptions = DoomOptions::new();
-        let doom_option_names: Vec<&str> = vec![
-            "-devparm",
-            "-nomonsters",
-            "-respawn",
-            "-fast",
-            "-debugfile",
-            "-shdev",
-            "-regdev",
-            "-comdev",
-            "-altdeath",
-            "-deathmatch",
-            "-cdrom",
-            "-turbo",
-            "-wart",
-            "-warp",
-            "-file",
-            "-playdemo",
-            "-timedemo",
-            "-skill",
-            "-episode",
-            "-timer",
-            "-avg",
-            "-statcopy",
-            "-record",
-            "-loadgame",
+        let doom_option_names: Vec<String> = vec![
+            "-devparm".to_string(),
+            "-nomonsters".to_string(),
+            "-respawn".to_string(),
+            "-fast".to_string(),
+            "-debugfile".to_string(),
+            "-shdev".to_string(),
+            "-regdev".to_string(),
+            "-comdev".to_string(),
+            "-altdeath".to_string(),
+            "-deathmatch".to_string(),
+            "-cdrom".to_string(),
+            "-turbo".to_string(),
+            "-wart".to_string(),
+            "-warp".to_string(),
+            "-file".to_string(),
+            "-playdemo".to_string(),
+            "-timedemo".to_string(),
+            "-skill".to_string(),
+            "-episode".to_string(),
+            "-timer".to_string(),
+            "-avg".to_string(),
+            "-statcopy".to_string(),
+            "-record".to_string(),
+            "-loadgame".to_string(),
         ];
 
         assert!(doom_options.options.iter().all(|option| {
-            doom_option_names.contains(&option.name) && !option.enabled() && option.values == None
+            doom_option_names.iter().any(|x| x == option.name)
+                && !option.enabled()
+                && option.values == None
         }));
         assert_eq!(doom_options.options.len(), doom_option_names.len());
     }
@@ -324,7 +365,7 @@ mod tests {
     #[test]
     fn test_set_options_works_with_no_cmd_args() {
         let mut doom_options: DoomOptions = DoomOptions::new();
-        let cmd_args: Vec<&str> = Vec::new();
+        let cmd_args: Vec<String> = Vec::new();
         set_options(&mut doom_options, cmd_args);
 
         assert!(doom_options
@@ -339,14 +380,14 @@ mod tests {
     #[should_panic]
     fn test_set_options_with_invalid_option() {
         let mut doom_options: DoomOptions = DoomOptions::new();
-        let cmd_args: Vec<&str> = vec![
-            "-devparm",
-            "-record",
-            "test",
-            "invalidOption",
-            "-wart",
-            "1",
-            "1",
+        let cmd_args: Vec<String> = vec![
+            "-devparm".to_string(),
+            "-record".to_string(),
+            "test".to_string(),
+            "invalidOption".to_string(),
+            "-wart".to_string(),
+            "1".to_string(),
+            "1".to_string(),
         ];
         set_options(&mut doom_options, cmd_args);
     }
@@ -358,20 +399,20 @@ mod tests {
     #[should_panic]
     fn test_set_options_with_too_many_option_values() {
         let mut doom_options: DoomOptions = DoomOptions::new();
-        let cmd_args: Vec<&str> = vec![
-            "-devparm",
-            "-record",
-            "test",
-            "-wart",
-            "1",
-            "1",
-            "1",
-            "-shdev",
-            "-file",
-            "file1",
-            "path/to/file2",
-            "/path/to/file3",
-            "-comdev",
+        let cmd_args: Vec<String> = vec![
+            "-devparm".to_string(),
+            "-record".to_string(),
+            "test".to_string(),
+            "-wart".to_string(),
+            "1".to_string(),
+            "1".to_string(),
+            "1".to_string(),
+            "-shdev".to_string(),
+            "-file".to_string(),
+            "file1".to_string(),
+            "path/to/file2".to_string(),
+            "/path/to/file3".to_string(),
+            "-comdev".to_string(),
         ];
         set_options(&mut doom_options, cmd_args);
     }
@@ -383,18 +424,18 @@ mod tests {
     #[should_panic]
     fn test_set_options_with_not_enough_option_values() {
         let mut doom_options: DoomOptions = DoomOptions::new();
-        let cmd_args: Vec<&str> = vec![
-            "-wart",
-            "1",
-            "-devparm",
-            "-record",
-            "test",
-            "-shdev",
-            "-file",
-            "file1",
-            "path/to/file2",
-            "/path/to/file3",
-            "-comdev",
+        let cmd_args: Vec<String> = vec![
+            "-wart".to_string(),
+            "1".to_string(),
+            "-devparm".to_string(),
+            "-record".to_string(),
+            "test".to_string(),
+            "-shdev".to_string(),
+            "-file".to_string(),
+            "file1".to_string(),
+            "path/to/file2".to_string(),
+            "/path/to/file3".to_string(),
+            "-comdev".to_string(),
         ];
         set_options(&mut doom_options, cmd_args);
     }
@@ -405,7 +446,15 @@ mod tests {
     #[should_panic]
     fn test_set_options_with_custom_options() {
         let mut doom_options: DoomOptions = DoomOptions::new();
-        let cmd_args: Vec<&str> = vec!["-test", "-test1value", "1", "-test3value", "1", "2", "3"];
+        let cmd_args: Vec<String> = vec![
+            "-test".to_string(),
+            "-test1value".to_string(),
+            "1".to_string(),
+            "-test3value".to_string(),
+            "1".to_string(),
+            "2".to_string(),
+            "3".to_string(),
+        ];
 
         set_options(&mut doom_options, cmd_args);
     }
@@ -416,19 +465,19 @@ mod tests {
     fn test_set_existing_doom_option_values() {
         let mut doom_options = DoomOptions::new();
 
-        let cmd_args: Vec<&str> = vec![
-            "-wart",
-            "1",
-            "1",
-            "-devparm",
-            "-record",
-            "test",
-            "-shdev",
-            "-file",
-            "file1",
-            "path/to/file2",
-            "/path/to/file3",
-            "-comdev",
+        let cmd_args: Vec<String> = vec![
+            "-wart".to_string(),
+            "1".to_string(),
+            "1".to_string(),
+            "-devparm".to_string(),
+            "-record".to_string(),
+            "test".to_string(),
+            "-shdev".to_string(),
+            "-file".to_string(),
+            "file1".to_string(),
+            "path/to/file2".to_string(),
+            "/path/to/file3".to_string(),
+            "-comdev".to_string(),
         ];
 
         set_options(&mut doom_options, cmd_args);
@@ -472,6 +521,90 @@ mod tests {
             .eq("file1 path/to/file2 /path/to/file3"));
         assert!(file.enabled());
 
+        let comdev_option: Option<&DoomOption> = get_option_by_name(&doom_options, "-comdev");
+        assert!(comdev_option.is_some());
+
+        let comdev: &DoomOption = comdev_option.unwrap();
+        assert!(comdev.values.as_ref().unwrap().eq("true"));
+        assert!(comdev.enabled());
+    }
+
+    // Read arguments in from a response file denoated
+    // by @response_file_path
+    // The response file will have one option and its values per line
+    // Will drop all options passed into the command line before it
+    // Will keep all options passed into the command line after it
+    // If the option line in a response file has an invalid char
+    // such as tab or |, (anything less than ascii code for space or
+    // anything more than ascii code z) the line will be ignored and
+    // not be processed
+    #[test]
+    fn test_set_doom_options_from_response_file() {
+        let mut doom_options = DoomOptions::new();
+
+        let cmd_args: Vec<String> = vec![
+            // Should be dropped since
+            // it is before @responsefile
+            "-wart".to_string(),
+            "1".to_string(),
+            "1".to_string(),
+            "-devparm".to_string(),
+            "-record".to_string(),
+            "test".to_string(),
+            // For this test, response file will
+            // have:
+            //      -shdev
+            //      -file file1 path/to/file2 /path/to/file3
+            //      -|test 1 2 3
+            //      -(tab)test2 3 4 5
+            //-shdev and -file are valid and should be processed
+            //-test and test2 have invalid chars so they should be ignored
+            // Should keep -comdev since its after @responsefile
+            "@tests/resource/responsefile".to_string(),
+            "-comdev".to_string(),
+        ];
+
+        set_options(&mut doom_options, cmd_args);
+
+        // Before response file. Should not be set
+        let wart_option: Option<&DoomOption> = get_option_by_name(&doom_options, "-wart");
+        assert!(wart_option.is_some());
+
+        let wart: &DoomOption = wart_option.unwrap();
+        assert!(wart.values.as_ref().is_none());
+
+        let devparm_option: Option<&DoomOption> = get_option_by_name(&doom_options, "-devparm");
+
+        let devparm: &DoomOption = devparm_option.unwrap();
+        assert!(devparm.values.as_ref().is_none());
+
+        let record_option: Option<&DoomOption> = get_option_by_name(&doom_options, "-record");
+        assert!(record_option.is_some());
+
+        let record: &DoomOption = record_option.unwrap();
+        assert!(record.values.as_ref().is_none());
+
+        // Start response file
+        let shdev_option: Option<&DoomOption> = get_option_by_name(&doom_options, "-shdev");
+        assert!(shdev_option.is_some());
+
+        let shdev: &DoomOption = shdev_option.unwrap();
+        assert!(shdev.values.as_ref().unwrap().eq("true"));
+        assert!(shdev.enabled());
+
+        let file_option: Option<&DoomOption> = get_option_by_name(&doom_options, "-file");
+        assert!(file_option.is_some());
+
+        let file: &DoomOption = file_option.unwrap();
+        assert!(file
+            .values
+            .as_ref()
+            .unwrap()
+            .eq("file1 path/to/file2 /path/to/file3"));
+        assert!(file.enabled());
+        // End response File
+
+        // After response file
         let comdev_option: Option<&DoomOption> = get_option_by_name(&doom_options, "-comdev");
         assert!(comdev_option.is_some());
 
