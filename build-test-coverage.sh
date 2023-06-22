406a064f3f958c07bcb32e5de310ed6a30770ef4
Zachary Alan Johnson <zjohnson366@gmail.com>
2023-06-22 00:58:37 -0400

    Cleanup build-test-coverage.sh

--- build-test-coverage.sh
+++ build-test-coverage.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-#rm -rf ./*.profraw
-#rm -rf target/debug/coverage
-
-#rustup component add llvm-tools-preview
 cargo llvm-cov > /dev/null 2>&1
 if [ $? -eq 0 ]; then
         printf "cargo llvm-cov already installed...\n"
@@ -13,19 +9,3 @@ else
 fi
 
 cargo llvm-cov --html
-
-#if command -v grcov; then
-#        printf "grcov already installed...\n"
-#else
-#        printf "Installing grcov....\n"
-#        cargo install --force grcov
-#fi
-
-#export RUSTFLAGS="-Cinstrument-coverage"
-
-#cargo build
-
-#export LLVM_PROFILE_FILE="zz-doom-%p-%m.profraw"
-#cargo test
-
-#grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
